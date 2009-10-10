c23de290f40842c108b1cb134dcb11bb3f6acd96
Dobrica Pavlinusic <dpavlin@meeepc.(none)>
2009-10-10 18:38:01 +0200

    install on clean Debian box

---
+++ bin/install-debian.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -x
+
+echo "deb http://www.debian-multimedia.org sid main" > /etc/apt/sources.list.d/debian-multimedia.list
+apt-get update
+apt-get install libio-epoll-perl libdata-dump-perl libfile-slurp-perl libyaml-perl libjson-perl xdotool mplayer
