879051bc12760fa631798987a05090939b2fddb8
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-10-18 23:50:54 +0200

    attempt to fix shell montage

--- bin/slides-monrage.sh
+++ bin/slides-monrage.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-dir=www/media/s
+dir=media/_editing/s
 
-ls -d $dir/* | grep x | sed "s,$dir/*,," | sort -n -r | while read size ; do
-	echo "# $size";
-
-	if [ ! -e $dir/bars.png ] ; then
-		convert media/SMPTE_Color_Bars.svg -geometry $size $dir/bars.png
-	fi
-
-	montage -geometry +1+1 -frame 3 -label %f $dir/$size/* $dir/$size.png
+function resize() {
+	size=$1
+	gm montage -geometry +1+1 -frame 3 -label %f $dir/$size/* $dir/$size.png
 	qiv $dir/$size.png
-done
+}
+
+#resize hires
+resize 1
+resize 2
+resize 4
 
