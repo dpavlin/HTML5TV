440c275fa54469f7f8052262b8e5ad90d5f5d044
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-10-19 20:40:42 +0200

    hopefully useful slide overview layout

--- lib/HTML5TV/Slides.pm
+++ lib/HTML5TV/Slides.pm
@@ -15,24 +15,72 @@ sub new {
 	bless $self, $class;
 }
 
-my $overview = SDL::Surface->new( -name => 'media/_editing/s/overview.jpg' );
+our $app;
 
-my $app = SDL::App->new(
-	-width  => $overview->width(),
-	-height => $overview->height(),
-	-depth  => 24,
-	-title  => 'Slides',
-);
+my ( $x, $y ) = ( 0, 0 );
+my ( $w, $h );
 
-my $rect = SDL::Rect->new(
-	-height => $overview->height(),
-	-width  => $overview->width(),
-	-x      => 0,
-	-y      => 0,
-);
+my @factors = ( qw/ 4 4 4 4 1 2 2 4 4 4 4 / );
 
-$overview->blit( $rect, $app, $rect );
-$app->update( $rect );
+foreach my $path (
+	sort {
+		my $n_a = $1 if $a =~ m{(\d+)};
+		my $n_b = $1 if $b =~ m{(\d+)};
+		$n_a <=> $n_b || $a cmp $b
+	} glob("media/_editing/s/2/*")
+) {
+
+	my $factor = shift @factors || '4';
+	$path =~ s{/s/[124]/}{/s/$factor/};
+
+	my $slide = SDL::Surface->new( -name => $path );
+
+	my $rect = SDL::Rect->new(
+		-width  => $slide->width(),
+		-height => $slide->height(),
+		-x      => 0,
+		-y      => 0,
+	);
+
+	my $to = SDL::Rect->new(
+		-width  => $slide->width(),
+		-height => $slide->height(),
+		-x      => $x,
+		-y      => $y,
+	);
+
+	warn "$x $y $path\n";
+
+	if ( ! $app ) {
+		$w = $slide->width()  * $factor;
+		$h = $slide->height() * $factor * 2;
+
+		warn "window $w $h\n";
+
+		$app = SDL::App->new(
+			-width  => $w,
+			-height => $h,
+			-depth  => 24,
+			-title  => 'Slides',
+		);
+
+	}
+
+		
+	$slide->blit( $rect, $app, $to );
+	$app->update( $to );
+
+	$x += $slide->width();
+
+	if ( $x >= $w ) {
+		$x = 0;
+		$y += $slide->height();
+	}
+
+}
+
+warn "window $w $h\n";
+
 
 <STDIN>;
 
