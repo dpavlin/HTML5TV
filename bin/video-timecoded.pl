992c9cf2d010a57d7a566d6d45e1730afc61da81
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-10-22 14:41:18 +0200

    extract length using oggLength

--- bin/video-timecoded.pl
+++ bin/video-timecoded.pl
@@ -9,6 +9,12 @@ use File::Path qw(rmtree);
 my $length = 100;
 my $fps = 5;
 
+if ( my $original = shift @ARGV ) {
+	my $len_ms = `oggLength $original`;
+	$length = int( ( $len_ms + 500 ) / 1000 );
+	warn "$original\t$length s\n";
+}
+
 rmtree "/tmp/blank";
 mkdir "/tmp/blank";
 
@@ -31,7 +37,7 @@ foreach my $pos ( 0 .. $length * $fps ) {
 	my $path = sprintf $frame_fmt, $pos;
 	$im->Write( filename => $path );
 #	warn "# $hh $mm $ss $path ", -s $path, $/ if $t % $fps == 0;
-	print STDERR '.' if $pos % $fps == 0;
+	print STDERR $pos % 60 * %fps == 0 ? '*' : '.' if $pos % $fps == 0;
 }
 
 print STDERR "\n";
