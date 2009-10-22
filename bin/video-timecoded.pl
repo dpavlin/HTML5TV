ee68ded85690c7f22f5c2708826599f62b4b695c
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-10-22 13:52:51 +0200

    insert keyframes every second, better output

--- bin/video-timecoded.pl
+++ bin/video-timecoded.pl
@@ -7,10 +7,12 @@ use Graphics::Magick;
 use File::Path qw(rmtree);
 
 my $length = 100;
-my $fps = 10;
+my $fps = 5;
 
 rmtree "/tmp/blank";
 mkdir "/tmp/blank";
+
+my $frame_fmt = '/tmp/blank/f%08d.jpg';
 
 foreach my $pos ( 0 .. $length * $fps ) {
 
@@ -26,10 +28,16 @@ foreach my $pos ( 0 .. $length * $fps ) {
 		text => sprintf("%02d:%02d:%06.3f", $hh, $mm, $ss ),
 		fill => 'yellow',
 	);
-	my $path = sprintf "/tmp/blank/f%04d.jpg", $pos;
+	my $path = sprintf $frame_fmt, $pos;
 	$im->Write( filename => $path );
-	warn "# $hh $mm $ss $path ", -s $path, $/;
+#	warn "# $hh $mm $ss $path ", -s $path, $/ if $t % $fps == 0;
+	print STDERR '.' if $pos % $fps == 0;
 }
 
+print STDERR "\n";
+
 #system "oggSlideshow -f $fps -o /tmp/blank.ogv -d 20000 -e -l $fps -t p /tmp/blank/f*";
-system "ffmpeg2theora --framerate $fps -o /tmp/blank.ogv /tmp/blank/f%04d.jpg";
+system "ffmpeg2theora --framerate $fps --keyint $fps -o /tmp/blank.ogv $frame_fmt";
+
+rmtree "/tmp/blank";
+
