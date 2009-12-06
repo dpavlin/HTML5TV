540edabdc63540ad1d6af7c5e03b6bea6c9e5b6a
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-12-06 19:39:11 +0100

    mirror presentation and video

--- media/JSConf.eu-Node.js/www.sh
+++ media/JSConf.eu-Node.js/www.sh
@@ -1,2 +1,2 @@
-#wget -nc http://blip.tv/file/get/Jsconfeu-RyanDahlNodejs958.mov?referrer=blip.tv&source=1
 wget -nc http://s3.amazonaws.com/four.livejournal/20091117/jsconf.pdf
+wget -nc http://blip.tv/file/get/Jsconfeu-RyanDahlNodejs958.mov?referrer=blip.tv&source=1
