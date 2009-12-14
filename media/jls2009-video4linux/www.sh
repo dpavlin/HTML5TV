e0b2deeb4dcefcd753d694a156993550e93f862d
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-12-14 19:50:44 +0100

    Hans Verkuil: Video4Linux: Past, Present and Future

---
+++ media/jls2009-video4linux/www.sh
@@ -0,0 +1,3 @@
+wget -nc http://events.linuxfoundation.org/images/stories/slides/jls09/jls09_verkuil.pdf
+echo "vimeo original: http://vimeo.com/download/video:12954491?v=2&e=1260814935&h=3e2ed4a2004bdd32753a09007090a5f5&uh=e217acdc1104b85dcd737f7a082b0cfc"
+cclive http://vimeo.com/8141503
