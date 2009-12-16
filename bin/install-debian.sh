a9c51e8ba8ac751d09fe89499aa886dbbbd24f63
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-12-16 23:16:29 +0100

    use pdftoppm from poppler-utils

--- bin/install-debian.sh
+++ bin/install-debian.sh
@@ -5,6 +5,6 @@ apt-get update
 apt-get install libio-epoll-perl libdata-dump-perl libfile-slurp-perl libyaml-perl libjson-perl libhtml-tree-perl \
 	libgraphics-magick-perl libsdl-perl libxml-feedpp-perl \
 	libtheora-dev libgd2-noxpm-dev libvorbis-dev \
-	xdotool mplayer ffmpeg2theora
+	xdotool mplayer ffmpeg2theora poppler-utils
 
 cpan HTML::ResolveLink
