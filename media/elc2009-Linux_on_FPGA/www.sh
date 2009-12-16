559aea86e5728ab1bc90671d1a43e8862d36cf35
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-12-16 23:39:47 +0100

    migrate to www.sh

---
+++ media/elc2009-Linux_on_FPGA/www.sh
@@ -0,0 +1,2 @@
+wget -nc -O ELC2009_Embedded_Linux_on_FPGAs_for_fun_and_profit.pdf http://www.celinuxforum.org/CelfPubWiki/ELC2009Presentations?action=AttachFile&do=get&target=ELC2009_Embedded_Linux_on_FPGAs_for_fun_and_profit.pdf
+wget -nc http://free-electrons.com/pub/video/2009/elc/elc2009-williams-linux-on-fpga.ogv
