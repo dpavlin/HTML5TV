76bcbaf138078b89fdd661872c853c848f47b5d0
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-01-01 17:05:50 +0100

    Philippe Oechslin: Exposing Crypto Bugs through reverse engineering

---
+++ media/26c3-crypto-reverse-engineering/www.sh
@@ -0,0 +1,2 @@
+# wget http://mirror.hellercom.de/ccc/events/congress/26c3/mp4/26c3-3703-de-exposing_crypto_bugs_through_reverse_engineering.mp4
+wget -nc http://events.ccc.de/congress/2009/Fahrplan/attachments/1462_26c3_oechslin_crypto_bugs.pdf
