57ae38f159bf04a14a7e969f0e3393c3f127a48c
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-02-02 14:54:28 +0100

    use ffmpeg2theora --sync to fix A/V sync

--- media/10gen-mongodb/www.sh
+++ media/10gen-mongodb/www.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -x
 
 wget -nc http://assets.leadit.us/mysql/MongoDB-10gen-CEO-Dwight-Merriman-presenting-at-NYC-MySQL-Group-at-Sun-Microsystems.pdf
-wget http://blip.tv/file/get/Emagazine-10genCEODoubleClickCoFounderPresentingMongoDBHighPerforma244.flv -O Emagazine-10genCEODoubleClickCoFounderPresentingMongoDBHighPerforma244.flv
-ffmpeg2theora -p preview Emagazine-10genCEODoubleClickCoFounderPresentingMongoDBHighPerforma244.flv
+wget -nc http://blip.tv/file/get/Emagazine-10genCEODoubleClickCoFounderPresentingMongoDBHighPerforma244.flv -O Emagazine-10genCEODoubleClickCoFounderPresentingMongoDBHighPerforma244.flv
+ffmpeg2theora --sync -p videobin Emagazine-10genCEODoubleClickCoFounderPresentingMongoDBHighPerforma244.flv
+#ffmpeg -i Emagazine-10genCEODoubleClickCoFounderPresentingMongoDBHighPerforma244.flv -s 512x288 -vcodec libtheora -acodec libvorbis Emagazine-10genCEODoubleClickCoFounderPresentingMongoDBHighPerforma244.ogv
