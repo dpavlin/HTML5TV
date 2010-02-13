205e504190692c9b6f4fe3ae28bcfb5adeffb245
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-02-13 11:59:33 +0100

    Eric Day, Brian Aker: Map/Reduce and Queues for MySQL Using Gearman

---
+++ media/mysql2009-gearman/www.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -x
+
+wget http://assets.en.oreilly.com/1/event/21/Map_Reduce%20and%20Queues%20for%20MySQL%20Using%20Gearman%20Presentation.pdf
+cclive http://www.youtube.com/watch?v=xDRDu1g270c
+ffmpeg2theora youtube_xDRDu1g270c.flv
