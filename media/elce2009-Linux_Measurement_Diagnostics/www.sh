7ae5dadd7c725d709f5364a0592c18be6cf10658
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-03-27 19:17:25 +0100

    Frank Rowand: A Survey of Linux Measurement and Diagnostics Tools

---
+++ media/elce2009-Linux_Measurement_Diagnostics/www.sh
@@ -0,0 +1,4 @@
+wget -nc -O survey_of_linux_measurement_and_diagnostic_tools.pdf \
+	'http://tree.celinuxforum.org/CelfPubWiki/ELCEurope2009Presentations?action=AttachFile&do=get&target=survey_of_linux_measurement_and_diagnostic_tools.pdf'
+wget -nc http://free-electrons.com/pub/video/2009/elce/elce2009-rowand-measurement-diagnostic-tools.ogv && \
+	ffmpg2theora -p preview elce2009-rowand-measurement-diagnostic-tools.ogv
