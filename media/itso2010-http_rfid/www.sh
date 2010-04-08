e0cecdd11e9960db6eb68bf5101321cab64beb66
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-04-08 21:30:24 +0200

    encode video in ogv using ffmpeg2theora

--- media/itso2010-http_rfid/www.sh
+++ media/itso2010-http_rfid/www.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -nc -O 18274566.mp4 'http://s3.amazonaws.com/videos.vimeo.com/182/745/18274566.mp4?AWSAccessKeyId=0MG9FJ7W1FZ5MBJXGS02&Expires=1270465957&Signature=7iY%2BAFWLrTj3JKpO7%2BSe2GZmxEc%3D'
+wget -nc -O 18274566.mp4 'http://s3.amazonaws.com/videos.vimeo.com/182/745/18274566.mp4?AWSAccessKeyId=0MG9FJ7W1FZ5MBJXGS02&Expires=1270465957&Signature=7iY%2BAFWLrTj3JKpO7%2BSe2GZmxEc%3D' && ffmpeg2theora -p preview 18274566.mp4
 wget -nc http://www.rot13.org/~dpavlin/presentations/itsof2010_rest_rfid.pdf
