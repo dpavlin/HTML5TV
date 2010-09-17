5c4a5fd413bcdb5c109a1751af3f3f5efb20290e
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-09-17 13:20:15 +0200

    download video and convert to ogv, convert ppt to pdf

--- media/sql-VoltDB/www.sh
+++ media/sql-VoltDB/www.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 wget -nc http://technocation.org/files/doc/2010_09_VoltDB.ppt
-#clive http://www.youtube.com/watch?v=Gjg8LTEaLbc
+unoconv 2010_09_VoltDB.ppt
+clive http://www.youtube.com/watch?v=Gjg8LTEaLbc
+ffmpeg2theora VoltDBforSQLDevelopersbyTimCallaghanattheSept2010BostonMySQLUserGroup.flv
