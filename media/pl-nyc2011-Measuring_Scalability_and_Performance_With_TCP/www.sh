6a58660ae57976c0d8eb2d52153812a18fa6a134
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-06-21 19:50:29 +0200

    Baron Schwartz: Measuring Scalability and Performance With TCP

---
+++ media/pl-nyc2011-Measuring_Scalability_and_Performance_With_TCP/www.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#get_flash_videos http://blip.tv/play/hKlBgsOWcQA.html
+wget -nc http://www.percona.com/files/presentations/percona-live/nyc-2011/PerconaLiveNYC2011-Extracting-Performance-and-Scalability-Metrics-From-TCP.pdf
